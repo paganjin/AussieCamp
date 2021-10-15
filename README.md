34fd4b15a33e72f0173ffb46b7e21cf64e51ab57
Tianyou JIN <ohjinjinjin@gmail.com>
2021-10-16 00:05:10 +1100

    prepare for deploy

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AussieCamp
+# Vicamp
+
+The goal for this website is to share nice camping and hiking places for people who love nature.
--- src/app.js
+++ src/app.js
@@ -1,17 +1,18 @@
 const express = require('express')
+require('./db/mongoose')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const mongoSanitize = require('express-mongo-sanitize')
+const helmet = require('helmet')
 const path = require('path')
-require('./db/mongoose')
+const MongoDBStore = require('connect-mongo')
 
 const ExpressError = require('./utils/ExpressError')
-
 const User = require('./models/user')
-
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
@@ -25,9 +26,22 @@ app.set('views', path.join(__dirname, 'views'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static('public'))
+app.use(mongoSanitize({ replaceWith: '_' }))
+
+const store = MongoDBStore.create({
+    mongoUrl: process.env.MONGODB_URL,
+    secret: '!this!should!be!a!better!secret!',
+    touchAfter: 24 * 60 * 60
+})
+
+store.on('error', function (err) {
+    console.log('SESSION STORE ERROR', err)
+})
 
 const sessionConfig = {
-    secret: 'secret',
+    store,
+    name: 'session',
+    secret: '!this!should!be!a!better!secret!',
     resave: false,
     saveUninitialized: true,
     cookie: {
@@ -36,12 +50,58 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig))
 app.use(flash())
+app.use(helmet())
+
+const scriptSrcUrls = [
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+    "https://kit.fontawesome.com/",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net",
+]
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+]
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+]
+const fontSrcUrls = []
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/douqbebwk/",
+                "https://images.unsplash.com/",
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+)
 
 app.use(passport.initialize())
 app.use(passport.session())
 passport.use(new LocalStrategy(User.authenticate()))
+
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
